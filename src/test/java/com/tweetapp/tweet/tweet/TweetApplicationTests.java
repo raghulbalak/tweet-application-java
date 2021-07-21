f383352281705540630b691b12d7e7a99605fd5f
raghulbalak <86151975+raghulbalak@users.noreply.github.com>
2021-07-21 17:56:54 +0530

    Update TweetApplicationTests.java

--- src/test/java/com/tweetapp/tweet/tweet/TweetApplicationTests.java
+++ src/test/java/com/tweetapp/tweet/tweet/TweetApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TweetApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
 }
